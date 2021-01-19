61ba54119efbc34bbc27da9504fd475334d6bc12
Seonho Cho <jshone144@gmail.com>
2021-01-19 15:57:42 +0900

    update my code

--- test_baseball_game.py
+++ test_baseball_game.py
@@ -228,7 +228,3 @@ class TestBaseballGame(unittest.TestCase):
                     ball += 1
         result = [strikes, ball]
         return result
-
-
-if __name__ == '__main__':
-    unittest.main()
